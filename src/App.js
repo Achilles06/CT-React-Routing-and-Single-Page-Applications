86623226f8b2b72fff68530cb28df6bb127c8e79
Achilles06 <92751219+Achilles06@users.noreply.github.com>
2024-08-26 21:10:52 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Home from './components/Home';
 import BrowseCharacters from './components/BrowseCharacters';
 import CharacterDetails from './components/CharacterDetails';
 import Comics from './components/Comics';
+import NotFound from './components/NotFound';
+import NavBar from './components/NavBar';
 
 const App = () => {
     return (
         <Router>
             <div className="app">
-                <h1>Comic Book Library Management System</h1>
+                <NavBar />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/browse-characters" element={<BrowseCharacters />} />
                     <Route path="/character-details/:id" element={<CharacterDetails />} />
                     <Route path="/comics" element={<Comics />} />
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
             </div>
         </Router>
--- src/components/BrowseCharacters.jsx
+++ src/components/BrowseCharacters.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const BrowseCharacters = () => {
     const [characters, setCharacters] = useState([]);
@@ -25,11 +26,13 @@ const BrowseCharacters = () => {
         <div className="character-grid">
             {characters.map((character) => (
                 <div key={character.id} className="character-item">
-                    <img
-                        src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-                        alt={character.name}
-                    />
-                    <h3>{character.name}</h3>
+                    <Link to={`/character-details/${character.id}`}>
+                        <img
+                            src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                            alt={character.name}
+                        />
+                        <h3>{character.name}</h3>
+                    </Link>
                 </div>
             ))}
         </div>
